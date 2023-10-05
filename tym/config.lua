ce570f0fd9e7b0603eef58276a9976f81cf1ec60
Spektrum-interface <marcomariocostantino64@gmail.com>
2023-10-05 19:23:33 +0200

    added basic tym configuration

---
+++ tym/config.lua
@@ -0,0 +1,11 @@
+local tym = require('tym')
+tym.set('font', "Iosevka Nerd Font 10")
+
+tym.set_config({
+    shell = '/usr/bin/nu',
+    cursor_shape = 'block',
+    autohide = true,
+    color_foreground = white,
+})
+color_foreground = '#FFFFFF'
+color_background = '#000000'
