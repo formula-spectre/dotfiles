13a0ce817bb7405df2613e581a43542ef5fd2a3b
Spektrum-interface <marcomariocostantino64@gmail.com>
2023-10-17 07:44:19 +0200

    touched up nushell's config in home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,23 +2,12 @@
 let
   USER = builtins.getEnv "USER";
   BROWSER = "librewolf";
-in
-{
-   home = {
-     username = "${USER}";
-     homeDirectory = "/home/${USER}";
-     stateVersion = "22.05";
-      sessionPath = [
-        "/home/${USER}/.config/emacs/bin"
-        "/home/${USER}/.nix-profile/bin/"
-        "/home/${USER}/.local/bin"
-        "/home/${USER}/.local/bin/bw/"
-     ];
-     sessionVariables = {
+  myEnvVars = {
        NIX_PATH="/home/${USER}/.nix-defexpr/channels";
        TERM="xterm";
        BROWSER="${BROWSER}";
        XDG_CONFIG_HOME="/home/${USER}/.config";
+       XDG_DATA_HOME="/home/${USER}/.local/share";
        ZSH_COMPDUMP = "${config.xdg.cacheHome}/zsh/zcompdump";
        EMACSDIR = "${config.xdg.configHome}/emacs";
        DOOMDIR = "${config.xdg.configHome}/doom.d";
@@ -37,58 +26,79 @@ in
        XCOMPOSECACHE="${config.xdg.cacheHome}/X11/compose";
        #XKB_DEFAULT_OPTIONS="ctrl:nocaps";
        #XKB_DEFAULT_LAYOUT="\"it(us)\"";
-       PASSWORD_STORE_GPG_OPTS="--default-recipient cloud.strife@tuta.io";
-       MOZ_ENABLE_WAYLAND=1;
-     };
+       PASSWORD_STORE_GPG_OPTS="\" --default-recipient cloud.strife@tuta.io\""
+;
+       MOZ_ENABLE_WAYLAND="1";
+  };
+in
+{
+  imports = [
+    ./zsh.nix
+  ];
+   home = {
+     username = "${USER}";
+     homeDirectory = "/home/${USER}";
+     stateVersion = "22.05";
+      sessionPath = [
+        "/home/${USER}/.config/emacs/bin"
+        "/home/${USER}/.nix-profile/bin/"
+        "/home/${USER}/.local/bin"
+        "/home/${USER}/.local/bin/bw/"
+     ];
+     sessionVariables = myEnvVars;
    };
    programs = {
      nushell = {
        enable = true;
-       extraConfig = "
-$env.config.completions.algorithm =  \"fuzzy\"
-$env.config.show_banner = false
-       ";
+        extraConfig = "
+ $env.config = {
+   completions: {
+       algorithm:  \"fuzzy\"
+   }
+   hooks : {
+     env_change: {
+         PWD: []
+     }
+   }
+   show_banner: false
+ }
+ $env.config.hooks.env_change.PWD = (
+     $env.config.hooks.env_change.PWD | append (source ~/.local/share/nu/nu_scripts/hooks/direnv/config.nu)
+ )
+use ~/.local/share/nu/nu_scripts/custom-completions/pass/pass-completions.nu *
+                     ";
+       extraEnv = "
+$env.PATH = (
+    $env.PATH
+    | split row (char esep)
+    | prepend ($env.HOME | path join \".local\" \"bin\")
+    | prepend ($env.HOME | path join \".nix-profile\" \"bin\")
+    | uniq
+)
 
+#$env.NU_LIB_DIRS = [
+#            ($env.XDG_DATA_HOME | path join \"nu\" \"nu_scripts\")
+#            ]
+                  ";
        shellAliases = {
+         tr="transmission-remote";
+         trl="transmission-remote --list";
+         tra="transmission-remote --add";
          cp="~/.local/bin/ucp -rvg";
          jobs="jobs -p";
-         #ls="lsd -F --color=never";
          grep="rg";
-         #md="mkdir -p";
          e="emacsclient -ct";
-         s6-rc-user="s6-rc -l /tmp/\$\{USER\}/s6-rc";
+         s6-rc-user="s6-rc -l /tmp/s6-rc";
          cat="bat";
          yt-mp3="youtube-dl -x --audio-format mp3 --prefer-ffmpeg";
          #yt-mp4="";
          k="kubectl";
          gc="git clone";
+         r="rm -rvf";
+         md="mkdir";
+         termbin="nc termbin.com 9999";
        };
-       environmentVariables =  {
-         NIX_PATH="/home/${USER}/.nix-defexpr/channels";
-         TERM="xterm";
-         BROWSER="${BROWSER}";
-         XDG_CONFIG_HOME="/home/${USER}/.config";
-         ZSH_COMPDUMP = "${config.xdg.cacheHome}/zsh/zcompdump";
-         EMACSDIR = "${config.xdg.configHome}/emacs";
-         DOOMDIR = "${config.xdg.configHome}/doom.d";
-         ZSHZ_DATA = "${config.xdg.dataHome}/zsh/zshzdb";
-         XMONAD_CONFIG_HOME="${config.xdg.configHome}/xmonad";
-         XMONAD_DATA_DIR="${config.xdg.configHome}/xmonad";
-         XMONAD_CACHE_DIR="${config.xdg.configHome}/xmonad";
-         CABAL_CONFIG="${config.xdg.configHome}/cabal/config";
-         CABAL_DIR="${config.xdg.configHome}/cabal";
-         CARGO_HOME="${config.xdg.dataHome}/cargo";
-         GHCUP_USE_XDG_DIRS="true";
-         GNUPGHOME="${config.xdg.dataHome}/gnupg";
-         GTK2_RC_FILES="${config.xdg.configHome}/gtk-2.0/gtkrc";
-         LESSHISTFILE="${config.xdg.cacheHome}/less/history";
-         ICEAUTHORITY="${config.xdg.cacheHome}/ICEauthority";
-         XCOMPOSECACHE="${config.xdg.cacheHome}/X11/compose";
-         #XKB_DEFAULT_OPTIONS="ctrl:nocaps";
-         #XKB_DEFAULT_LAYOUT="it(us)";
-         PASSWORD_STORE_GPG_OPTS="\"--default-recipient cloud.strife@tuta.io\"";
-         MOZ_ENABLE_WAYLAND="1";
-       };
+       environmentVariables = myEnvVars;
 
      };
      direnv = {
@@ -159,151 +169,6 @@ $env.config.show_banner = false
      #      cp = "ucp -rgv";
      #   };
      # };
-     zsh = {
-       completionInit = "autoload -U compinit && compinit -u";
-       envExtra = ''
-source /etc/profile
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$HOME/.local/share"
-export PATH=$PATH:~/.nix-profile/bin:~/.local/bin:~/.local/bin/bw #FIXME!
-                  '';
-       profileExtra = ''
-if [  "$(tty)" = "/dev/tty1" ] ; then
-  mkdir -p /tmp/''${USER}/{service,s6-log}
-  touch /tmp/''${USER}/s6-log/current
-  mkfifo /tmp/''${USER}/s6-log/logfifo
-  setsid -f \
-      fdmove -c 3 2 \
-          redirfd -wnb 1 "/tmp/''${USER}/s6-log/logfifo" \
-          fdmove -c 2 1  \
-              s6-envdir ~/.config/s6u/envdir \
-              s6-svscan -X3 /tmp/''${USER}/service
-  [ -d /tmp/''${USER}/s6-rc ] ||
-    redirfd -w 1 "/tmp/''${USER}/s6-log/logfifo" \
-        s6-rc-init -c ''${HOME}/.config/s6u/rc/compiled -l /tmp/''${USER}/s6-rc /tmp/''${USER}/service
-fi
-                      '';
-       dotDir = ".config/zsh.nix";
-       defaultKeymap = "emacs";
-       history = {
-         size = 10000;
-         save = 10000;
-         path = "${config.xdg.dataHome}/zsh_history";
-         ignoreDups = true;
-         ignoreSpace = true;
-         ignorePatterns = [ "rm *" "kill *" "transmission*" "pkill *"];
-         share = true;
-       };
-       enable = true;
-       enableAutosuggestions = true;
-       enableCompletion = true;
-       syntaxHighlighting.enable = true;
-       autocd = true;
-       shellGlobalAliases = {
-         termbin="nc termbin.com 9999";
-         r="rm -rvf";
-       };
-       shellAliases = {
-         cp="ucp -rvg";
-         jobs="jobs -p";
-         ls="lsd -F --color=never";
-         grep="rg";
-         md="mkdir -p";
-         e="emacsclient -ct";
-         s6-rc-user="s6-rc -l /tmp/\$\{USER\}/s6-rc";
-         cat="bat";
-         yt-mp3="youtube-dl -x --audio-format mp3 --prefer-ffmpeg";
-         yt-mp4="";
-         k="kubectl";
-         ll="ls -l";
-         l1="ls -1";
-         gc="git clone";
-       };
-        initExtraFirst = (
-  "
- [[ $TERM == \"tramp\" ]] && unsetopt zle && PS1=\'$ \' && return
- [[ $TERM == \"dumb\" ]] && unsetopt zle && PS1=\'$ \' && return
-  "
-       );
-       initExtra = (
-         "
- source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
- source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
- source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
- source ${pkgs.nix-zsh-completions}/share/zsh/plugins/nix/nix-zsh-completions.plugin.zsh
- #source ${pkgs.fzf}/share/fzf/completion.zsh
- #source ${pkgs.fzf}/share/fzf/key-bindings.zsh
- source /usr/share/fzf/key-bindings.zsh
- #source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
- source /usr/share/zsh/site-functions/fzf-tab.zsh
- source ${pkgs.zsh-fzf-tab}/share/fzf-tab/lib/zsh-ls-colors/ls-colors.zsh
- source ${pkgs.deer}/share/zsh/site-functions/deer
- source \$\{ZDOTDIR\}/functions
- setopt globdots
- autoload -U deer
- zle -N deer
- bindkey '\\eOP' deer
- export PROMPT=$PROMPT$pr_shortkeys
-        "
-        );
-        plugins = [
-          {
-            name = "zsh-z";
-            file = "zsh-z.plugin.zsh";
-            src = builtins.fetchGit {
-              url = "https://github.com/agkozak/zsh-z";
-            };
-          }
-        ];
-     }; # zsh ends ehre
-     # fzf = {
-     #   enable = true;
-     #   enableZshIntegration = true;
-     #};
    }; # programs ends here
-   xdg = {configFile."zsh.nix/functions".text = ''
 
- agent() {
-   eval $(ssh-agent)
-   ssh-add "$HOME/.ssh/id_ed25519" }
-
- function cd() {
-    emulate -LR zsh
-    builtin cd $@ &&
-    lsd -F --color=never --group-dirs=first -A }
-
- function popd(){
-   emulate -LR zsh
-   builtin popd $@ &&
-   lsd -F --color=never --group-dirs=first -A }
-
- function pushd(){
-   emulate -LR zsh
-   builtin pushd $@ &&
-   lsd -F --color=never --group-dirs=first -A }
-
- function z(){
-   emulate -LR zsh
-   z $@ &&
-   ls -F --color=never --group-directories-first -A }
-
- curlhab () { curl -X POST \
-     --header "Content-Type: text/plain" \
-     --header "Accept: application/json" -d "$2" "http://192.168.1.4:8080/rest/items/$1"}
-
- clearcaches(){
-   set -ex
-   doas execlineb -Pc 'if { sync } pipeline { echo 3 } tee /proc/sys/vm/drop_caches' }
-
- aura (){
-      printf "reminder: aura does not support doas yet, so i'm using sudo..\n"
-      /usr/bin/sudo aura --unsuppress $* }
-
-extractgentoo (){
-      tar xpvf $* --xattrs-include='*.*' --numeric-owner
-      echo "remember to copy resolv.conf" }
-
- '';
-   };
 }
