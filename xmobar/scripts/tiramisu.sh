f260c9fe6e1861197965fd099e30d7dc68db6467
Spektrum-interface <marcomariocostantino64@gmail.com>
2023-11-08 06:41:49 +0100

    renamed s6u to s6-rc

--- xmobar/scripts/tiramisu.sh
+++ xmobar/scripts/tiramisu.sh
@@ -8,7 +8,7 @@
 
 cat /tmp/noti-log
 # tiramisu -o "#sourceNEW_LABEL#summaryNEW_LABEL#body" |
-#     while read -r noti; do
-#      echo "$noti" | sed 's/NEW_LABEL/\ /g' | cut -c1-50
-#       herbe "$(echo "$noti" | sed 's/NEW_LABEL/\n/
+#      while read -r noti; do
+#       echo "$noti" | sed 's/NEW_LABEL/\ /g' | cut -c1-50
+#        herbe "$(echo "$noti" | sed 's/NEW_LABEL/\n/')"
 #       done
